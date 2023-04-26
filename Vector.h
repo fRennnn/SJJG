68af370c2d5c514c77eb3965e0c84f839d3d0d05
fRen <102521678+fRennnn@users.noreply.github.com>
2023-04-26 12:13:19 +0800

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -116,7 +116,70 @@ template<typename T> static bool lt(T &a,T &b){return a<b;}
 template<typename T> static bool eq(T *a,T *b){return eq(*a,*b);}
 template<typename T> static bool eq(T &a,T &b){return a == b;}
 
-template<typename T>
+template<typename T>//无序向量的顺序查找: 返回最后一个元素e的位置;失败时返回lo-1
 Rank Vector<T>::find(T const& e,Rank lo,Rank hi)const{
-    while( (lo<hi--) && (e != _elem[hi]) )
+    while( (lo<hi--) && (e != _elem[hi]) )//从后往前，顺序查找
+    return hi;//若hi < lo，则意味失败;否则hi即命中元素的秩
+}
+ 
+template<typename T>
+Rank Vector<T>::insert(Rank r,T const & e){
+    expand();
+    for(int i = _size;i>r;i--) _elem[i] = _elem[i-1];
+    _elem[r] = e;_size++;
+    return r;
+} 
+
+template<typename T> int Vector<T>::remove(Rank lo,Rank hi){
+    if(lo==hi) return 0;
+    while(hi<_size) _elem[lo++] = _elem[hi++];
+    _size = lo;
+    shrink();
+    return hi-lo;
+}
+
+template<typename T> T Vector<T>::remove(Rank r){
+    T e = _elem[r];
+    remove(r,r+1);
+    return e;
+}
+//唯一化
+template<typename T> int Vector<T>::deduplicate(){//删除无序向量中重复元素(高效版)
+    int oldSize = _size;//记录原规模
+    Rank i = 1;
+    while(i<_size)
+    (find(_elem[i],0,i)<0?
+    i++:remove(i));
+    return oldSize - _size;//向量规模变化量，即被删除元素总数
+}
+
+template<typename T> void Vector<T>::traverse(void (*visit)(T&))
+{for(int i = 0;i<_size;i++)visit(_elem[i]);}//遍历向量
+
+template<typename T> template<typename VST>
+void Vector<T>::traverse(VST& visit)
+{for(int i =0;i<_size;i++)visit(_elem[i]);}
+
+template<typename T>  struct Increase
+{ virtual void operator() (T &e) {e++;} };
+
+template<typename T>  void increase(Vector<T> &V)
+{ V.traverse(Increase<T>()); }
+
+template<typename T> 
+int Vector<T>::Disordered()
+{
+    int n = 0;
+    for(int i = 1;i<_size;i++)
+    if(_elem[i-1]>_elem[i])n++;
+    return n;
+}
+
+template<typename T> int Vector<T>::uniquify(){
+    Rank i = 0,j = 0;
+    while(++j < _size)
+      if(_elem[i] != _elem[j])
+      _elem[++i] = _elem[j];
+    _size = ++i;shrink();
+    return j-i;
 }
